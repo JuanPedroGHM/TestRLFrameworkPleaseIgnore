Critic supports GPu class, small changes to fix gradient calculation through GPu and kernel class

trlfpi/gp/critic.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 from scipy.optimize import minimize
@@ -5,6 +6,7 @@ from scipy.optimize import minimize
 from typing import Any
 
 from .gp import GP
+from .gpu import GPu
 from .kernel import Kernel
 from trlfpi.memory import GPMemory
 
@@ -16,17 +18,27 @@ class Critic():
                  mean: Any = None,
                  memory: int = 1000,
                  optim_freq: int = 100,
-                 bruteFactor: int = 5,
+                 bruteFactor: int = 10,
                  bGridSize: int = 5,
-                 bPoolSize: int = 8):
+                 bPoolSize: int = 8,
+                 device: str = None):
 
-        self.memory = GPMemory(inputSpace, maxSize=memory)
-        kernel = Kernel.RBF(1.0, np.array([1.0 for i in range(inputSpace)]))
-        self.model = GP(kernel,
-                        meanF=mean,
-                        sigma_n=0.1,
-                        bGridSize=bGridSize,
-                        bPoolSize=bPoolSize)
+        self.memory = GPMemory(inputSpace, maxSize=memory, device=device)
+        if device:
+            kernel = Kernel.RBF(1.0, [1.0 for i in range(inputSpace)], gpu=True)
+            self.model = GPu(kernel,
+                             meanF=mean,
+                             sigma_n=0.1,
+                             bGridSize=bGridSize,
+                             bPoolSize=bPoolSize,
+                             device=device)
+        else:
+            kernel = Kernel.RBF(1.0, [1.0 for i in range(inputSpace)])
+            self.model = GP(kernel,
+                            meanF=mean,
+                            sigma_n=0.1,
+                            bGridSize=bGridSize,
+                            bPoolSize=bPoolSize)
         self.optim_freq = optim_freq
         self.updates = 0
         self.bruteFactor = bruteFactor

trlfpi/gp/gp.py
@@ -85,7 +85,7 @@ class GP():
 
     def brute(self):
 
-        lengthBounds = (np.max(self.X_TRAIN, axis=0) - np.min(self.X_TRAIN, axis=0)) * 10
+        lengthBounds = (np.max(self.X_TRAIN, axis=0) - np.min(self.X_TRAIN, axis=0)) * 100
         ranges = [(0, 250)]
         ranges.extend([(0, bound) for bound in lengthBounds])
         stepSizes = np.array([(range[1] - range[0]) / self.bGridSize for range in ranges])
@@ -119,6 +119,7 @@ class GP():
             res = optim.minimize(self.logLikelihood, params, bounds=bounds)
             self.kernel.params = res.x
 
+        print(self.kernel.params)
         K = self.kernel(X, X)
         try:
             self.L, self.alpha = self.L_alpha(K, X, Y)

trlfpi/gp/gpu.py
@@ -69,7 +69,7 @@ class GPu():
         alpha = torch.cholesky_solve(Y - self.priorMean(X), L)
         return L, alpha
 
-    def logLikelihood(self, params: torch.Tensor) -> torch.Tensor:
+    def logLikelihood(self, params: torch.Tensor = None) -> torch.Tensor:
         K = self.kernel(self.X_TRAIN, self.X_TRAIN, customParams=params)
         L, alpha = self.L_alpha(K, self.X_TRAIN, self.Y_TRAIN)
         result = 0.5 * self.Y_TRAIN.T @ alpha + torch.sum(torch.log(torch.diag(L)))
@@ -79,10 +79,10 @@ class GPu():
     def brute(self):
 
         lengthBounds = (torch.max(self.X_TRAIN, dim=0)[0] - torch.min(self.X_TRAIN, dim=0)[0]) * 10
-        ranges = [(0, 250)]
-        ranges.extend([(0, bound) for bound in lengthBounds])
-        stepSizes = torch.tensor([(range[1] - range[0]) / self.bGridSize for range in ranges])
-        grid = [torch.arange(range[0], range[1], step) for range, step in zip(ranges, stepSizes)]
+        ranges = [(0.0, torch.tensor(100.0))]
+        ranges.extend([(0.0, bound) for bound in lengthBounds])
+        stepSizes = [(r[1] - r[0]) / self.bGridSize for r in ranges]
+        grid = [torch.arange(r[0], r[1].item(), step.item()) for r, step in zip(ranges, stepSizes)]
         grid = torch.stack(torch.meshgrid(grid)).T.reshape(-1, len(ranges))
         llArray = list(map(self.logLikelihood, grid))
         bestParams = grid[torch.tensor(llArray).argmin()]
@@ -100,19 +100,19 @@ class GPu():
             self.kernel.params = params
 
         if fineTune:
-            params = [torch.tensor(self.kernel.params, requires_grad=True)]
-            lbfgsOptim = torch.optim.LBFGS(params, max_iter=100, lr=1e-4)
+            tParams = [torch.tensor(self.kernel.params, requires_grad=True)]
+            lbfgsOptim = torch.optim.LBFGS(tParams, max_iter=100, lr=1e-4)
 
             def closure():
                 lbfgsOptim.zero_grad()
-                ll = self.logLikelihood(params[0])
+                ll = self.logLikelihood(tParams[0])
                 ll.backward()
                 return ll
 
             lbfgsOptim.step(closure)
 
             # res = (self.logLikelihood, params, bounds=bounds)
-            self.kernel.params = params[0]
+            self.kernel.params = tParams[0]
 
         K = self.kernel(X, X)
         self.L, self.alpha = self.L_alpha(K, X, Y)

trlfpi/gp/kernel_util.py
@@ -32,6 +32,8 @@ def rbf_f_t(x0: Tensor, x1: Tensor, params):
     if torch.any(length == 0):
         return torch.zeros((x0.shape[0], x1.shape[0]))
 
-    dist = torch.cdist(x0 / length, x1 / length, p=2)
+    a = x0 / length
+    b = x1 / length
+    dist = torch.cdist(a, b, p=2)
     K = theta**2 * torch.exp(-.5 * dist)
     return K